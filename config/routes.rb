4ab09a6710807253104f440370a163e919f6dbf9
Siwei Shen <sg552sg552@gmail.com>
2012-03-12 08:54:39 +0800

    added rails-admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Bubu::Application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
+  devise_for :users
+
   resources :categories
 
   # The priority is based upon order of creation:
@@ -51,6 +55,7 @@ Bubu::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => "categories#index"
 
   # See how all your routes lay out with "rake routes"
 
