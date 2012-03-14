8aa2b70d1cf0e53eedfebef0ae782ee66665cc72
Siwei Shen <sg552sg552@gmail.com>
2012-03-15 01:23:49 +0800

    added test case for user.admin?

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@
 
 FactoryGirl.define do
   factory :user do
+    sequence(:email) { |n| "this#{n}@email.com" }
+    password "lalalalalala"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,8 @@
 require 'spec_helper'
 
 describe User do
+  it "should be admin? " do
+    create(:user, :role => User::ROLE_ADMIN ).admin?.should == true
+    create(:user).admin?.should == false
+  end
 end
