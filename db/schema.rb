a306324f98e2ab0ddb094bd0e329c417370719f1
Siwei Shen <sg552sg552@gmail.com>
2012-03-14 18:43:35 +0800

    added user registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120313142120) do
+ActiveRecord::Schema.define(:version => 20120314090950) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -77,8 +77,8 @@ ActiveRecord::Schema.define(:version => 20120313142120) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                                 :default => "",     :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",     :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -87,8 +87,9 @@ ActiveRecord::Schema.define(:version => 20120313142120) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                            :null => false
-    t.datetime "updated_at",                                            :null => false
+    t.datetime "created_at",                                                :null => false
+    t.datetime "updated_at",                                                :null => false
+    t.string   "role",                                  :default => "user"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
