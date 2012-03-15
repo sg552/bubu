3651317484283b4b584c7de53adce3e8181f8a86
Siwei Shen <sg552sg552@gmail.com>
2012-03-15 17:38:30 +0800

    added generic_item#price_scope()

--- app/models/generic_item.rb
+++ app/models/generic_item.rb
@@ -2,4 +2,12 @@ class GenericItem < ActiveRecord::Base
   has_many :specific_items
   has_many :images
   has_many :comments
+  NO_PRICE_SCOPE = "暂无定价"
+
+  def price_scope
+    return NO_PRICE_SCOPE if specific_items.blank?
+    prices = specific_items.collect { | item| item.price.to_i }.sort - [0]
+    return "#{prices.first}" if prices.size == 1
+    return "#{prices.first}-#{prices.last}"
+  end
 end
--- spec/models/generic_item_spec.rb
+++ spec/models/generic_item_spec.rb
@@ -24,4 +24,20 @@ describe GenericItem do
     @generic_item.specific_items.size.should == 2
   end
 
+  describe "price_scope " do
+    it "should be blank if there's no specific_items" do
+      @generic_item.price_scope.should == GenericItem::NO_PRICE_SCOPE
+    end
+    it "should between 2 - 4 for multiple price " do
+      create(:specific_item, :generic_item_id => @id, :price => 2)
+      create(:specific_item, :generic_item_id => @id, :price => 4)
+      @generic_item.price_scope.should == "2-4"
+    end
+    it "should between 2 for single price" do
+      create(:specific_item, :generic_item_id => @id, :price => 2)
+      @generic_item.price_scope.should == "2"
+    end
+
+  end
+
 end
