e0c378a058eb63a6ecd8c6a3aae0cc5865e45ecf
Siwei Shen <sg552sg552@gmail.com>
2012-03-20 07:46:43 +0800

    implemented category menu on root page

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,12 @@ class Category < ActiveRecord::Base
   PRINCIPLE_BY_USAGE = "by_usage"
   PRINCIPLE_BY_SHAPE= "by_shape"
   PRINCIPLE_BY_AGE = "by_age"
+
+  # define scope :by_usage, :by_shape, :by_age
+  [PRINCIPLE_BY_AGE, PRINCIPLE_BY_USAGE, PRINCIPLE_BY_SHAPE].each do |by|
+    scope by.to_sym, where(:principle => by)
+  end
+
   def generic_items(limit=nil)
     generic_items = GenericItem.where("category_id_#{principle} = #{id}")
     limit.blank? ?  generic_items.all : generic_items.limit(limit)
