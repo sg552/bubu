197f8212e23380a54249c699ec81b77e419ab95e
Siwei Shen <sg552sg552@gmail.com>
2012-03-27 18:58:44 +0800

    used i18n for front pages(landing page, generic_item#show page), with default language as Chinese

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+
+  # for those user whose browser is not using our default_locale, e.g. a Chinese using English broser,
+  # just like me. :)
+  def set_locale
+    I18n.locale = params[:local] || I18n.default_locale
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_path, :alert => exception.message
   end
--- config/initializers/bubu_constants.rb
+++ config/initializers/bubu_constants.rb
@@ -1,1 +1,6 @@
+# backend constants (for ruby)
 ADMIN_LAYOUT = "admin_layout"
+
+
+# frontend (for HTML element)
+WEBSITE_TITLE = "布布德智用品导购信息网"
