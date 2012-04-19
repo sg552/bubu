519b09c9e8c407a9b09797429899e4b136cf4d87
Siwei Shen <sg552sg552@gmail.com>
2012-04-19 11:48:45 +0800

    implemented : register

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -3,30 +3,30 @@ class Devise::RegistrationsController < DeviseController
   prepend_before_filter :authenticate_scope!, :only => [:edit, :update, :destroy]
 
   ## GET /resource/sign_up
-  #def new
-  #  resource = build_resource({})
-  #  respond_with resource
-  #end
+  def new
+    resource = build_resource({})
+    respond_with resource
+  end
 
   ## POST /resource
-  #def create
-  #  build_resource
+  def create
+    build_resource
 
-  #  if resource.save
-  #    if resource.active_for_authentication?
-  #      set_flash_message :notice, :signed_up if is_navigational_format?
-  #      sign_in(resource_name, resource)
-  #      respond_with resource, :location => after_sign_up_path_for(resource)
-  #    else
-  #      set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-  #      expire_session_data_after_sign_in!
-  #      respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-  #    end
-  #  else
-  #    clean_up_passwords resource
-  #    respond_with resource
-  #  end
-  #end
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_in(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
 
   # GET /resource/edit
   def edit
