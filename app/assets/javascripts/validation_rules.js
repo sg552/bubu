092be19f5c295547c654298ae83a6dfb62b35300
Siwei Shen <sg552sg552@gmail.com>
2012-04-21 09:34:30 +0800

    added validation for users#register

--- app/assets/javascripts/validation_rules.js
+++ app/assets/javascripts/validation_rules.js
@@ -4,3 +4,14 @@ rules_of_login = [
   "valid_email,user[email], 请输入有效格式的邮件地址"
 ]
 
+rules_of_register = [
+  "required,user[email], 请输入邮件地址",
+  "required,user[login], 请输入昵称",
+  "required,user[password], 请输入密码",
+  "required,user[password_confirmation], 请再次输入密码",
+  "valid_email,user[email], 请输入有效格式的邮件地址",
+  "same_as,user[password],user[password_confirmation], 两次密码需要一致",
+  "length>=6,user[password], 密码不能小于6位",
+  "length>=6,user[password_confirmation], 密码不能小于6位."
+]
+
